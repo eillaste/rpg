32af80def3e487596318d633c811eec69f9477b9
Erik Illaste <erik.illaste@gmail.com>
2018-10-29 22:30:38 +0200

    'first'

---
+++ src/components/Thing.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Thing extends Component {
+	state = {};
+
+	handleClick() {
+		this.setState({ yoro: this.state.text });
+	}
+
+	changeText(e) {
+		this.setState({ text: e.target.value });
+	}
+
+	render() {
+		return (
+			<div>
+				<input onChange={this.changeText.bind(this)} />
+				<button onClick={this.handleClick.bind(this)}>Click me</button>
+				<h1>{this.state.text}</h1>
+			</div>
+		);
+	}
+}
+
+export default Thing;
